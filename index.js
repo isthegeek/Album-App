464de4094e73aba18c2747bf70a420d8f28dacf3
Vijay Nandwani <born2bgeeky@gmail.com>
2017-12-19 19:58:57 +0530

    Name Change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('AlbumApp', () => App);
+AppRegistry.registerComponent('AwesomeProject', () => App);
